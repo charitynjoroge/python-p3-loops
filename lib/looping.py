82f1828a8a06022c8b0c618d5bd359a459ad48ef
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-05-21 13:30:03 +0300

    update

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for countdown in range(10, 0, -1):
+        print(countdown)
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+happy_new_year()
+
+
+def square_integers(nums):
+    squared_nums = [num ** 2 for num in nums]
+    return squared_nums
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+    
